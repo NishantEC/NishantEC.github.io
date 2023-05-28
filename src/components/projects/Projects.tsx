700cd4581c734c26b17a1e6aacf61ac1b10ffa28
Nishantec <guptanishant1307@gmail.com>
2023-05-28 22:49:09 +0530

    few fixes

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -15,44 +15,51 @@ import {
 function Projects() {
   const projectsData = [
     {
-      heading: "OneStop Delivery Rider App",
+      heading: "OSD Rider & Vendor Apps Bundle",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+        "This isa Bundle of two apps, one for the rider and one for the vendor.",
       points: [
-        "Lorem ipsum dolor sitg elit. Quisquam, voluptatum.",
-        "Lorem met consectetur adipisicing elit. Quisquam, voluptatum.",
-        "Lorem ipsum dolor sit amet consquam, voluptatum.",
+        "The rider app is used by the delivery person to deliver the orders to the customers.",
+        "The vendor app allows restaurant owners to manage their orders and menu.",
+        "The Apps keep the delivery person's location and Vendor's status in real-time.",
+
+
       ],
-      techStack: ["React", "Redux", "Typescript", "Sass"],
-      githubLink: "",
+      techStack: ["Mobile-App","AngularJs", "Ionic Framework", "Typescript", "Sass"],
+      githubLink: null,
+      playStoreLink: "https://play.google.com/store/apps/developer?id=Bleedingedge+Technologies",
+      liveLink:null,
       imgUrl: project2Img,
     },
     {
       heading: "Bills Manager",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+        "The Bills Manager is a web app that helps you keep track of your bills and expenses.",
       points: [
-        "Lorem ipsum dolor sitg elit. Quisquam, voluptatum.",
-        "Lorem met consectetur adipisicing elit. Quisquam, voluptatum.",
-        "Lorem ipsum dolor sit amet consquam, voluptatum.",
+        "The app allows you to add your bills and expenses and view them in a chart.",
+        "The App also allows you to check your monthly expenses on chart",
       ],
-      techStack: ["React", "Redux", "ChartJs", "Sass"],
-      githubLink: "",
+      techStack: ["WebApp","React", "Redux", "ChartJs", "Sass"],
+      githubLink: "https://github.com/NishantEC/bills-manager",
+      playStoreLink: null,
+      liveLink: "https://billsmanager-ng.vercel.app/",
       imgUrl: project1Img,
     },
 
     {
       heading: "Pomodoro V2",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+        "The Pomodoro V2 is a web app that helps you manage your time and be more productive.",
 
       points: [
-        "Lorem ipsum dolor sitg elit. Quisquam, voluptatum.",
-        "Lorem met consectetur adipisicing elit. Quisquam, voluptatum.",
-        "Lorem ipsum dolor sit amet consquam, voluptatum.",
+        "The app has a timer that helps you manage your time for the sessions.",
+
+
       ],
       techStack: ["React", "Redux"],
-      githubLink: "",
+      githubLink: "https://github.com/NishantEC/pomodoro",
+      playStoreLink: null,
+      liveLink:"https://pomodoro-ng.vercel.app/",
       imgUrl: project3Img,
     },
   ];
@@ -119,18 +126,39 @@ function Projects() {
               ))}
             </div>
             <div className="links">
-              <div className="github-link">
+              {
+                project.githubLink && <div className="github-link">
+
+                <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
                 <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-                </svg>
-              </div>
-              <div className="live-link">
-                Try Demo
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+              </svg>
+                </a>
+            </div>
+              }
+              {
+                project.playStoreLink && <div className="playstore-link">
+                  <a href={project.playStoreLink} target="_blank" rel="noopener noreferrer">
+                  <svg clip-rule="evenodd"
+                  fill-rule="evenodd"
+                  stroke-linejoin="round"
+                  stroke-miterlimit="2"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  xmlns="http://www.w3.org/2000/svg"><path d="m8.306 6.987 2.449-2.449L2.999.257a2.032 2.032 0 0 0-1.983.005l7.29 6.725zM.287.95c-.178.291-.28.624-.28.977v12.146c0 .342.093.668.261.953l7.331-7.331L.287.95zM14.002 6.33l-2.336-1.289-2.625 2.624 3.217 2.967 1.745-.963c.628-.348 1.004-.972 1.004-1.67a1.894 1.894 0 0 0-1.005-1.669zM8.334 8.373.983 15.724a2.06 2.06 0 0 0 1.021.278c.34 0 .682-.086.995-.259l8.337-4.601-3.002-2.769z"></path></svg>
+                  </a>
+                </div>
+              }
+              {
+                project.liveLink && <div className="live-link">
+                  <a href={project.liveLink} target="_blank" rel="noopener noreferrer">
+                Live Link
                 <svg
                   clip-rule="evenodd"
                   fill-rule="evenodd"
@@ -146,7 +174,9 @@ function Projects() {
                     fill-rule="nonzero"
                   />
                 </svg>
+                </a>
               </div>
+              }
             </div>
           </div>
           <div className="project-img">
